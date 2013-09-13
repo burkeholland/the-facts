bbf1ac5576f853b3bdf66776b82ab272d68304e4
Burke Holland <burkeholland@gmail.com>
2013-09-13 16:07:25 -0500

    initial commit

---
+++ the-facts-app/scripts/app.js
@@ -0,0 +1,89 @@
+(function (global) {
+    var application,
+        api = "http://localhost:52787/";
+
+    global.app = {};
+
+    var randomJoke = function (explicit) {
+        var dfrd = $.Deferred();
+
+        $.get(api + "joke/" + (explicit ? "explicit" : "clean"), function (data) {
+            dfrd.resolve(data.JokeText);
+        });
+
+        return dfrd;
+    }
+
+    global.app.cleanModel = (function () {
+
+        var viewModel = kendo.observable({
+            joke: ""
+        });
+
+        var refresh = function () {
+            randomJoke(false).then(function (joke) {
+                viewModel.set("joke", joke);
+            });
+        };
+
+        return {
+            viewModel: viewModel,
+            refresh: refresh
+        }
+
+    }());
+
+    global.app.dashboardModel = (function () {
+
+        var init = function () {
+            $("#categoriesPie").kendoChart({
+                theme: "flat",
+                dataSource: {
+                    transport: {
+                        read: api + "dashboard/categories"
+                    }
+                },
+                legend: {
+                    visible: false
+                },
+                chartArea: {
+                    background: ""
+                },
+                seriesDefaults: {
+                    labels: {
+                        visible: true,
+                        background: "transparent",
+                        template: "#= category #: #= value #%"
+                    }
+                },
+                series: [{
+                    type: "pie",
+                    field: "Jokes",
+                    categoryField: "Description",
+                    startAngle: 150,
+
+                }]
+            });
+        }
+
+        return {
+            init: init
+        }
+
+    }());
+
+
+    global.app.jokesVIew = kendo.observable({
+        jokes: new kendo.data.DataSource({
+            transport: {
+                read: "api" + "jokes"
+            }
+        })
+    });
+
+    document.addEventListener("deviceready", function () {
+        application = new kendo.mobile.Application(document.body, { transition: "slide", skin: "flat" });
+    }, false);
+
+
+})(window);
